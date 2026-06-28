1a0718d0e9832c82e93dc8bd665727f4f3bcfcb7
Mohammad Abdulaziz <mohammad.abdulaziz8@gmail.com>
2026-06-28 15:05:36 +0100

    Cleaned up one smt/metis call. Replaced by an Isar script

--- Edmonds_Blossom_Algorithm/Graph_Quotient.thy
+++ Edmonds_Blossom_Algorithm/Graph_Quotient.thy
@@ -136,11 +136,64 @@ proof(induction p)
   then show ?case by simp
 next
   case (Cons a' p')
-  then show ?case
-    apply auto 
-    subgoal using split_list by fastforce
-    subgoal using assms(4) set_empty split_list by fastforce
-    by(metis Cons_eq_appendI all_not_in_conv append.left_neutral append_Cons assms(3) assms(4) empty_set set_ConsD)+
+  note IH = Cons.IH
+  show ?case
+  proof (cases "Q a'")
+    case True
+    note Qa' = True
+    \<comment> \<open>Since v1 \<noteq> v2, at least one of them lies in set p'\<close>
+    have "\<exists>y \<in> set p'. Q y"
+    proof -
+      have mem1: "v1 = a' \<or> v1 \<in> set p'" using Cons.prems(1) by simp
+      have mem2: "v2 = a' \<or> v2 \<in> set p'" using Cons.prems(2) by simp
+      show ?thesis
+      proof (cases "v1 = a'")
+        assume "v1 = a'"
+        then have "v2 \<noteq> a'" using Cons.prems(5) by simp
+        then have "v2 \<in> set p'" using mem2 by simp
+        with Cons.prems(4) show ?thesis by blast
+      next
+        assume "v1 \<noteq> a'"
+        then have "v1 \<in> set p'" using mem1 by simp
+        with Cons.prems(3) show ?thesis by blast
+      qed
+    qed
+    then obtain y where Qy: "Q y" and y_mem: "y \<in> set p'"
+      by blast
+    from y_mem obtain ys zs where decomp: "p' = ys @ y # zs"
+      by (meson split_list)
+    \<comment> \<open>a' # p' = [] @ a' # ys @ y # zs\<close>
+    show ?thesis
+      using Qa' Qy decomp
+      by (intro exI[of _ a'] exI[of _ y] exI[of _ "[]"] exI[of _ ys] exI[of _ zs]) simp
+  next
+    case False
+    \<comment> \<open>\<not> Q a' forces v1 \<noteq> a' and v2 \<noteq> a', so both land in p'\<close>
+    have v1_ne: "v1 \<noteq> a'"
+    proof
+      assume "v1 = a'"
+      with Cons.prems(3) have "Q a'" by simp
+      with False show False by simp
+    qed
+    have v2_ne: "v2 \<noteq> a'"
+    proof
+      assume "v2 = a'"
+      with Cons.prems(4) have "Q a'" by simp
+      with False show False by simp
+    qed
+    have v1_p': "v1 \<in> set p'" using Cons.prems(1) v1_ne by simp
+    have v2_p': "v2 \<in> set p'" using Cons.prems(2) v2_ne by simp
+    \<comment> \<open>Apply the induction hypothesis to p'\<close>
+    obtain w x qs rs ss where
+      decomp: "p' = qs @ w # rs @ x # ss" and Qw: "Q w" and Qx: "Q x"
+      and noQ_qs: "\<forall>x\<in>set qs. \<not> Q x"
+      using IH[OF v1_p' v2_p' Cons.prems(3) Cons.prems(4) Cons.prems(5)]
+      by blast
+    \<comment> \<open>a' # p' = (a' # qs) @ w # rs @ x # ss\<close>
+    show ?thesis
+      using decomp Qw Qx noQ_qs False
+      by (intro exI[of _ w] exI[of _ x] exI[of _ "a' # qs"] exI[of _ rs] exI[of _ ss]) auto
+  qed
 qed
 
 lemma one_mem_pure_suff:
