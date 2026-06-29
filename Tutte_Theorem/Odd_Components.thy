4a81d03405db29acdac69a13c7451dded87ab245
Mohammad Abdulaziz <mohammad.abdulaziz8@gmail.com>
2026-06-29 21:21:49 +0100

    Cleaned up one smt/metis call. Replaced by an Isar script

--- Tutte_Theorem/Odd_Components.thy
+++ Tutte_Theorem/Odd_Components.thy
@@ -2114,9 +2114,9 @@ proof -
                   by auto
               next
                 case (path1 v)
-                then show ?case 
-                  by (metis \<open>c \<in> C'\<close> \<open>c \<notin> C\<close> empty_iff empty_set in_own_connected_component 
-                      last_ConsL set_ConsD)
+                then have v_eq_c: "v = c" by simp
+                show ?case
+                  using v_eq_c \<open>c \<in> C'\<close> \<open>c \<notin> C\<close> in_own_connected_component by auto
               next
                 case (path2 v v' vs)
                 have "last (v' # vs) = c" 
